14555480e9ec252b8da2a3a8b16b9d008e73d03d
tanya2208 <34794680+tanya2208@users.noreply.github.com>
2020-02-03 16:56:55 +0300

    Landing

---
+++ landing/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true,
+    focusOnSelect: true,
+    dotsClass: "my-dots"
+  });
+});
+
+
+// Параллакс
